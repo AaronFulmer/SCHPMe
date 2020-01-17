260e56620e06d0591ddeabe7aa4330a5fa8c2e40
AaronFulmer <aar.fulmer@gmail.com>
2020-01-17 14:24:53 -0500

    Initial Commit: Splash and Home Screens

---
+++ iOSTest/HomeScreen.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  iOSTest
+//
+//  Created by Aaron Fulmer on 1/17/20.
+//  Copyright © 2020 Aaron Fulmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var journalButton: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
